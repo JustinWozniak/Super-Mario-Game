f3fd81f5d3c5f07c1e6135f2662591323eaa6086
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-01-27 12:32:04 -0800

    added new function spritesheet class, simplifies drawing sprites

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ loadImg('public/img/tiles.png')
         sprites.define('sky', 3, 23)
         for(let x = 0; x < 25; x++) {
             for(let y=0; y < 14; y++){
-                sprites.draw('sky', context, x * 16, y *16 )
+                sprites.drawTile('sky', context, x, y)
             }
         
     }
