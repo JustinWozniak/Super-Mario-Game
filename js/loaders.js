95503d4424040ac61922a684a63fe4165bfc619f
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-01-27 12:47:25 -0800

    refactored code, added new dependencys, now use json for levels

--- js/loaders.js
+++
@@ -1,9 +0,0 @@
-export function loadImg(url) {
-    return new Promise(resolve => {
-        const image = new Image()
-        image.addEventListener('load', () => {
-            resolve(image)
-        })
-        image.src = url
-    })
-}
