5ed1dbabc49247b3ff8321d9bedb27855b55de01
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-01-27 12:29:19 -0800

    added loops to draw sky on entire canvas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import SpriteSheet from './SpriteSheet.js'
-import {loadImg} from './loaders.js'
+import { loadImg } from './loaders.js'
 
 
 const canvas = document.getElementById('screen')
@@ -11,5 +11,14 @@ loadImg('public/img/tiles.png')
     .then(image => {
         const sprites = new SpriteSheet(image, 16, 16)
         sprites.define('ground', 0, 0)
+        sprites.define('sky', 3, 23)
+        for(let x = 0; x < 25; x++) {
+            for(let y=0; y < 14; y++){
+                sprites.draw('sky', context, x * 16, y *16 )
+            }
+        
+    }
+
+    
         sprites.draw('ground', context, 45, 62)
     })
