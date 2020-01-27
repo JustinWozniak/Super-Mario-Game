bbf45f16ee7f0771f155b7891b5b2afa26baa923
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-01-27 11:30:51 -0800

    Added sprite png, and setup my own module to draw it on the canvas

---
+++ js/main.js
@@ -0,0 +1,24 @@
+import SpriteSheet from './SpriteSheet.js'
+
+function loadImg(url) {
+    return new Promise(resolve => {
+        const image = new Image()
+        image.addEventListener('load', () => {
+            resolve(image)
+        })
+        image.src = url
+    })
+}
+
+
+const canvas = document.getElementById('screen')
+const context = canvas.getContext('2d')
+
+context.fillRect(0, 0, 50, 50)
+
+loadImg('public/img/tiles.png')
+    .then(image => {
+        const sprites = new SpriteSheet(image, 16, 16)
+        sprites.define('ground', 0, 0)
+        sprites.draw('ground', context, 45, 62)
+    })
