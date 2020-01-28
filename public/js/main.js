384afe6f220b448442fe4404de2e81b437c53b15
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-01-28 12:43:07 -0800

    moved some functions into new sprites folder, managed to get fox sprite to draw on canvas re-factored a few functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,43 @@
-import SpriteSheet from './SpriteSheet.js';
-import {loadImage, loadLevel} from './loaders.js';
+import Compositor from './Compositor.js';
+import {loadLevel} from './loaders.js';
+import {loadFoxSprite, loadBackgroundSprites} from './sprites.js';
+import {createBackgroundLayer} from './layers.js';
 
-// x1,x2 are first 2 parameters in the json file
-// y1,y2 are the second two parameters in json file
-function drawBackground(background, context, sprites) {
-    background.ranges.forEach(([x1, x2, y1, y2]) => {
-        for (let x = x1; x < x2; ++ x) {
-            for (let y = y1; y < y2; ++ y) {
-                sprites.drawTile(background.tile, context, x, y);
-            }
-        }
-    });
-}
-
-function loadBackgroundSprites() {
-
-    return loadImage('/img/tiles.png').then(image => {
-        const sprites = new SpriteSheet(image);
-        sprites.define('ground', 0, 0);
-        sprites.define('sky', 3, 23);
-        return sprites
-    })
-}
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-Promise.all([loadBackgroundSprites(), loadLevel('1-1')]).then(([sprites, level]) => {
-    level.backgrounds.forEach(bg => {
-        drawBackground(bg, context, sprites);
-    });
+
+function createSpriteLayer(sprite, pos) {
+    return function drawSpriteLayer(context) {
+        sprite.draw('idle', context, pos.x, pos.y);
+    };
+}
+
+
+Promise.all([
+    loadFoxSprite(),
+    loadBackgroundSprites(),
+    loadLevel('1-1'),
+])
+.then(([marioSprite, backgroundSprites, level]) => {
+    console.log('Level loader', level);
+
+    const comp = new Compositor();
+    comp.layers.push(createBackgroundLayer(level.backgrounds, backgroundSprites));
+
+    const pos = {
+        x: 64,
+        y: 64,
+    };
+
+    comp.layers.push(createSpriteLayer(marioSprite, pos));
+
+    function update() {
+        comp.draw(context);
+        pos.x += 2;
+        pos.y += 1;
+        requestAnimationFrame(update);
+    }
+
+    update();
 });
---
+++ public/js/sprites.js
@@ -0,0 +1,22 @@
+import SpriteSheet from './SpriteSheet.js';
+import {loadImage} from './loaders.js';
+
+export function loadFoxSprite() {
+    return loadImage('/img/foxSprite.png')
+    .then(image => {
+        const fox = new SpriteSheet(image, 25, 25);
+        fox.define('idle', 4, 292, 43, 60);
+        return fox;
+    });
+}
+
+export function loadBackgroundSprites() {
+    return loadImage('/img/tiles.png')
+    .then(image => {
+        console.log('Image loaded', image);
+        const sprites = new SpriteSheet(image, 16, 16);
+        sprites.defineTile('ground', 0, 0);
+        sprites.defineTile('sky', 3, 23);
+        return sprites;
+    });
+}
