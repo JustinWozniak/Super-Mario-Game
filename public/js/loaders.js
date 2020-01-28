0b9d569a63f873229cee96ff282b786d616c015f
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-01-28 11:23:55 -0800

    added favicon

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -2,9 +2,7 @@ export function loadImage(url) {
     return new Promise(resolve => {
         const image = new Image();
         image.addEventListener('load', () => {
-            //set a delay so no matter what image loads first
-            setTimeout(resolve,2000, image)
-            //resolve(image);
+            resolve(image);
         });
         image.src = url;
     });
