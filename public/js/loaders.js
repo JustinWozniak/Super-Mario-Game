7096cce4c8862d064f66c28abe1591a096fea70a
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-01-28 11:15:18 -0800

    Updated colours for our game, and did code cleanup. Level and image now load in a promise chain, so no matter what they load in proper order

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -2,7 +2,9 @@ export function loadImage(url) {
     return new Promise(resolve => {
         const image = new Image();
         image.addEventListener('load', () => {
-            resolve(image);
+            //set a delay so no matter what image loads first
+            setTimeout(resolve,2000, image)
+            //resolve(image);
         });
         image.src = url;
     });
--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,32 @@
 import SpriteSheet from './SpriteSheet.js';
 import {loadImage, loadLevel} from './loaders.js';
 
-const canvas = document.getElementById('screen');
-const context = canvas.getContext('2d');
-
+// x1,x2 are first 2 parameters in the json file
+// y1,y2 are the second two parameters in json file
 function drawBackground(background, context, sprites) {
     background.ranges.forEach(([x1, x2, y1, y2]) => {
-        for (let x = x1; x < x2; ++x) {
-            for (let y = y1; y < y2; ++y) {
+        for (let x = x1; x < x2; ++ x) {
+            for (let y = y1; y < y2; ++ y) {
                 sprites.drawTile(background.tile, context, x, y);
             }
         }
     });
 }
 
+function loadBackgroundSprites() {
 
-loadImage('/img/tiles.png')
-.then(image => {
-    const sprites = new SpriteSheet(image);
-    sprites.define('ground', 0, 0);
-    sprites.define('sky', 3, 23);
+    return loadImage('/img/tiles.png').then(image => {
+        const sprites = new SpriteSheet(image);
+        sprites.define('ground', 0, 0);
+        sprites.define('sky', 3, 23);
+        return sprites
+    })
+}
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
 
-    loadLevel('1-1')
-    .then(level => {
-        level.backgrounds.forEach(bg => {
-            drawBackground(bg, context, sprites);
-        });
+Promise.all([loadBackgroundSprites(), loadLevel('1-1')]).then(([sprites, level]) => {
+    level.backgrounds.forEach(bg => {
+        drawBackground(bg, context, sprites);
     });
 });
